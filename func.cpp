9d2980a18b48407dea1238ed854d513164fac0ec
lysenko_v <lysenko_v@itstep.academy>
2023-10-30 20:16:22 +0200

    1

--- func.cpp
+++ func.cpp
@@ -1,2 +1,22 @@
 #include "func.h"
 
+bool isLuckyNumber(int number) {
+    if (number < 100000 || number > 999999) {
+        return false;
+    }
+
+    int leftSum = 0;
+    int rightSum = 0;
+
+    for (int i = 0; i < 6; i++) {
+        if (i < 3) {
+            leftSum += number % 10;
+        }
+        else {
+            rightSum += number % 10;
+        }
+        number /= 10;
+    }
+
+    return leftSum == rightSum;
+}
--- maincpp.cpp
+++ maincpp.cpp
@@ -2,3 +2,17 @@
 
 
 
+int main() {
+    int number;
+    std::cout << "Enter a six-digit number: ";
+    std::cin >> number;
+
+    if (isLuckyNumber(number)) {
+        std::cout << "This is a lucky number!" << std::endl;
+    }
+    else {
+        std::cout << "This is not a lucky number." << std::endl;
+    }
+
+    return 0;
+}
