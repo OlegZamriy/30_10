d4dcc2bee4ed293039cf48218a71ffbebd2ac5ee
lysenko_v <lysenko_v@itstep.academy>
2023-10-30 19:52:55 +0200

    1

--- func.cpp
+++ func.cpp
@@ -1,2 +1,18 @@
 #include "func.h"
 
+double power(double base, int exponent) {
+    double result = 1.0;
+
+    if (exponent >= 0) {
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+    }
+    else {
+        for (int i = 0; i > exponent; i--) {
+            result /= base;
+        }
+    }
+
+    return result;
+}
--- maincpp.cpp
+++ maincpp.cpp
@@ -2,3 +2,17 @@
 
 
 
+int main() {
+    double base;
+    int exponent;
+
+    std::cout << "Enter the basis of the degree: ";
+    std::cin >> base;
+    std::cout << "Enter degree indicator: ";
+    std::cin >> exponent;
+
+    double result = power(base, exponent);
+    std::cout << base << " in degree " << exponent << " = " << result << std::endl;
+
+    return 0;
+}
